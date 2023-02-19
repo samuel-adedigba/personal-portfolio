651b759f1b8131e161dd1df6756d5a6c04355eb6
BeatrizNeaime <beatriz.neaime@outlook.com>
2023-02-19 18:12:18 -0300

    Feat: new projects added

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -13,7 +13,7 @@ const projectV = {
     visible: { opacity: 1, scale: 1 }
 }
 
-const Project = ({ title, subtitle }) => {
+const Project = ({ title, subtitle, path }) => {
     const projectTitle = title.split(" ").join("-").toLowerCase()
     const overlayer = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
     bg-pink z-30 flex flex-col justify-center items-center text-center p-16 text-deep-purple`;
@@ -22,12 +22,14 @@ const Project = ({ title, subtitle }) => {
             variants={projectV}
             className="relative"
         >
-            <div className={overlayer}>
-                <p className="text-2xl font-source-code" >{projectTitle}</p>
-                <p className="mt-7 text-center" >
-                    {subtitle}
-                </p>
-            </div>
+            <a href={path} target="_blank" rel="noopener noreferrer">
+                <div className={overlayer}>
+                    <p className="text-2xl font-source-code" >{projectTitle}</p>
+                    <p className="mt-7 text-center" >
+                        {subtitle}
+                    </p>
+                </div>
+            </a>
             <img src={require(`../assets/${projectTitle}.jpeg`)} alt={`${title} logo`} />
         </motion.div>
     )
@@ -73,11 +75,16 @@ const Projects = () => {
                     <div className="flex justify-center text-center items-center p-10 bg-pink-two max-w-[400px] max-h-[400px] text-2xl font-semibold font-source-code ">
                         usefull and responsive interfaces
                     </div>
-                    <Project title="Arte de Caderno" subtitle="Creating a portal with two other students for a project developed inside the College. We are still building it, using ReactJS for the front-end and Node.js for the back-end" />
-                    
-                    <Project title="BR finanças" subtitle="A finance controller named BR Finanças. Technologies used: HandleBars, NodeJS and MySQL." />
+                    <Project title="Arte de Caderno" subtitle="Creating a portal with two other students for a project developed inside the College. We are still building it, using ReactJS for the front-end and Node.js for the back-end" path="https://github.com/BeatrizNeaime/Arte-de-Caderno-Backup" />
 
-                    <Project title="Patinhas Felizes" subtitle="A menagement system for a fictitious animal shelter, where the workers can manage animal adoptions and donations. Technologies used: Handlebars, NodeJS and MySQL." />
+                    <Project title="BR finanças" subtitle="A finance controller named BR Finanças. Technologies used: HandleBars, NodeJS and MySQL." path="https://github.com/BeatrizNeaime/BR-financas" />
+
+                    <Project title="Patinhas Felizes" subtitle="A menagement system for a fictitious animal shelter, where the workers can manage animal adoptions and donations. Technologies used: Handlebars, NodeJS and MySQL." path="https://github.com/BeatrizNeaime/PatinhasFelizes" />
+
+                    <Project title="Advice" subtitle="In case you need an advice today... built with Advice Slip API" path="https://imaginative-paletas-b1d609.netlify.app" />
+
+                    <Project title="Splitter" subtitle="Application to calc the bill value per person. You need to input hte bill, the tip value and the number of people, and voilá!" path="https://fm-tip-calculator-pi.vercel.app" />
+
                 </motion.div>
             </div>
 
