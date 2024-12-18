7205dfce0f29e7e81baaf83d7ac6aa384ade7e3a
Samuel Adedigba <167442003+samuel-adedigba@users.noreply.github.com>
2024-12-18 21:45:10 +0100

    Update projects.js

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -7,7 +7,7 @@ export const projects = [
             pt: "Um site para um campeonato local de desenho e pintura por alunos de escolas públicas. Tema do meu Trabalho de Conclusão de Curso.",
             en: "A webapp that helps businesses and homes track their gas usage in real-time, and delivers to them before they run-out."
         },
-        technologies: ["React", "Styled-Components", "Redux-Toolkit", "Tailwind CSS"],
+        technologies: ["React","Styled-Components","Redux-Toolkit","Tailwind CSS"],
         path: "https://homefort.africa",
         git: "https://homefort.africa",
         img: "Hub_Dashboard"
@@ -16,7 +16,7 @@ export const projects = [
         title: "Homefort SolarBot",
         subtitle: {
             pt: "Um aplicativo para controle financeiro. Como este projeto foi feito como projeto interdisciplinar, o link do deploy não está disponível.",
-            en: "Homefort’s Machine Learning model recommends the most appropriate solar system configuration based on your load and budget "
+            en: "Homefort’s Machine Learning model recommends the most appropriate solar system configuration based on your load and budget."
         },
         technologies: ["NextJs", "Mobx", "Styled-Components"],
         path: "https://homefort.africa",
